0e32f4efc9043d61075b44502f3b030f8409560d
ZY <zy30651@qq.com>
2016-05-26 17:54:14 +0800

    添加了几个图片点击放大的方案

---
+++ likeReader/likeReader/Framework/Views/TBImageView/TBImageBrowser.h
@@ -0,0 +1,15 @@
+//
+//  TBImageBrowser.h
+//  likeReader
+//
+//  Created by ZY on 16/5/26.
+//  Copyright © 2016年 jack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TBImageBrowser : NSObject
+
++ (void)showImage:(UIImageView*)imageview;
+
+@end
---
+++ likeReader/likeReader/Framework/Views/TBImageView/TBPhotoGroupView.h
@@ -0,0 +1,62 @@
+//
+//  TBPhotoGroupView.h
+//  likeReader
+//
+//  Created by ZY on 16/5/26.
+//  Copyright © 2016年 jack. All rights reserved.
+//
+//
+//UIImageView *fromView = nil;
+//NSMutableArray *items = [NSMutableArray new];
+//NSArray *images = cell.layout.images;
+//
+//for (NSUInteger i = 0, max = images.count; i < max; i++) {
+//    UIImageView *imgView = cell.statusView.mediaView.imageViews[i];
+//    T1Media *img = images[i];
+//    YYPhotoGroupItem *item = [YYPhotoGroupItem new];
+//    item.thumbView = imgView;
+//    item.largeImageURL = img.mediaLarge.url;
+//    item.largeImageSize = img.mediaLarge.size;
+//    [items addObject:item];
+//    if (i == index) {
+//        fromView = imgView;
+//    }
+//}
+//YYPhotoGroupView *v = [[YYPhotoGroupView alloc] initWithGroupItems:items];
+//[v presentFromImageView:fromView toContainer:self.navigationController.view animated:YES completion:nil];
+//
+//
+//
+//
+//
+//
+
+#import <UIKit/UIKit.h>
+
+/// Single picture's info.
+@interface TBPhotoGroupItem : NSObject
+@property (nonatomic, strong) UIView *thumbView; ///< thumb image, used for animation position calculation
+@property (nonatomic, assign) CGSize largeImageSize;
+@property (nonatomic, strong) NSURL *largeImageURL;
+@end
+
+
+/// Used to show a group of images.
+/// One-shot.
+@interface TBPhotoGroupView : UIView
+@property (nonatomic, readonly) NSArray *groupItems; ///< Array<TBPhotoGroupItem>
+@property (nonatomic, readonly) NSInteger currentPage;
+@property (nonatomic, assign) BOOL blurEffectBackground; ///< Default is YES
+
+
+- (instancetype)init UNAVAILABLE_ATTRIBUTE;
+- (instancetype)initWithFrame:(CGRect)frame UNAVAILABLE_ATTRIBUTE;
++ (instancetype)new UNAVAILABLE_ATTRIBUTE;
+- (instancetype)initWithGroupItems:(NSArray *)groupItems;
+
+- (void)presentFromImageView:(UIView *)fromView
+                 toContainer:(UIView *)container
+                    animated:(BOOL)animated
+                  completion:(void (^)(void))completion;
+
+@end
